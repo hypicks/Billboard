1ab440b1112c6a880917160e05fddfc2920e7cd8
hypicks <44602385+hypicks@users.noreply.github.com>
2022-07-14 10:34:49 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+OAUTH_AUTHORIZE_URL = "https://accounts.spotify.com/authorize"
+OAUTH_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIPY_CLIENT_ID = "7e62d09556774ab592198e428b5ffc0f"
+SPOTIPY_CLIENT_SECRET = "19f4e93f2ac747fc8bf78eb71b7136df"
+SPOTIPY_REDIRECT_URI = "http://example.com"
+SPOTIPY_SCOPE = "playlist-modify-private"
+
+sp = spotipy.Spotify(
+auth_manager=SpotifyOAuth(
+client_id=SPOTIPY_CLIENT_ID,
+client_secret=SPOTIPY_CLIENT_SECRET,
+redirect_uri=SPOTIPY_REDIRECT_URI,
+scope=SPOTIPY_SCOPE,
+show_dialog=True,
+cache_path="token.txt"
+)
+)
+
+user_id = sp.current_user()["id"]
+
+print("Welcome to the Music Time Machine!")
+date = input("Enter the date you want to travel to (YYYY-MM-DD): ")
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
+response = requests.get(URL)
+billboard_data = response.text
+soup = BeautifulSoup(billboard_data, "html.parser")
+songs = soup.find_all(name="h3", id="title-of-a-story", class_="u-line-height-125")
+song_titles = [title.getText().strip("\n\t") for title in songs]
+artists = soup.find_all(name="span", class_="u-max-width-330")
+artist_names = [name.getText().strip("\n\t") for name in artists]
+song_and_artist = dict(zip(song_titles, artist_names))
+
+print(song_and_artist)
+print()
+print("Searching for songs on Spotify and creating new playlist...")
+
+song_uris = []
+for (song, artist) in song_and_artist.items():
+    try:
+        result = sp.search(q=f"track:{song} artist:{artist}", type="track")
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except:
+        pass
+
+print(f"Number of songs found: {len(song_uris)}")
+
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False, )
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+print(f"New playlist '{date} Billboard 100' successfully created on Spotify!")
